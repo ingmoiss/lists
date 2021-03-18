08a034a4529afc7051837602a157ccbc24799b09
Moisés Navarro <moi55navacor@gmail.com>
2021-03-17 20:07:27 -0600

    Added class Estudiante

---
+++ Listas/Estudiante.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    public class Estudiante
+    {
+        private string nombre;
+        private int nota1;
+        private int nota2;
+
+        public Estudiante(string nombre, int nota1, int nota2)
+        {
+            this.nombre = nombre;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public int Nota1
+        {
+            get { return this.nota1; }
+            set { this.nota1 = value; }
+        }
+
+        public int Nota2
+        {
+            get { return this.nota2; }
+            set { this.nota2 = value; }
+        }
+
+    }
+}
--- Listas/Program.cs
+++ Listas/Program.cs
@@ -10,6 +10,18 @@ namespace Listas
     {
         static void Main(string[] args)
         {
+            Estudiante oEstudiante1 = new Estudiante("Moises", 3, 4);
+            Estudiante oEstudiante2 = new Estudiante("Navarro", 3, 4);
+            Estudiante oEstudiante3 = new Estudiante("Cordero", 3, 4);
+
+            List<Estudiante> lisEstudiante = new List<Estudiante>
+            {
+                oEstudiante1, oEstudiante2, oEstudiante3
+            };
+
+            Console.WriteLine(lisEstudiante[0].Nombre + " "+ lisEstudiante[2].Nombre);
+
+            Console.ReadLine();
         }
     }
 }
